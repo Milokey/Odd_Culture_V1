8f9e6508d0e0aba07c906ee54ab5c7252b8fcd0a
ice cream <tiffanymblount@gmail.com>
2014-09-21 20:20:37 -0600

    Test

---
+++ src/main/java/com/oddculture/App.java
@@ -0,0 +1,14 @@
+package com.oddculture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+        System.out.println("Test");
+    }
+}
