8d4a6ada859ae72fa309817f1808e60234c42232
tblount <tblount@arrow.com>
2014-09-22 08:28:03 -0600

    Let's see if we can get the pom.xml up this time

--- src/main/java/com/oddculture/App.java
+++ src/main/java/com/oddculture/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
-        System.out.println("Test");
-        System.out.println("I kinda miss BitBucket now.");
+        System.out.println("Testing push. . . again. :(");
     }
 }
